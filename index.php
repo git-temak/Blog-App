6d9b3e138593522bfae821c025211ab7d63caacd
Temak <makindeoluwatimilehin@gmail.com>
2020-09-04 17:23:31 +0100

    core app
    skeleton of basic CRUD app with read feature

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+	//database connection
+	require('config/config.php');
+	require('config/connection.php');
+
+	// create query
+	$query = 'SELECT * FROM posts';
+
+	//get results
+	$result = mysqli_query($conn, $query);
+
+	//fetch data
+	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	// var_dump($posts);
+
+	//free result
+	mysqli_free_result($result);
+
+	//close connection
+	mysqli_close($conn);
+?>
+
+<?php require('inc/header.php'); ?>
+	<div class="container text-center my-5">
+		<h1>Posts</h1>
+		<div class="d-flex justify-content-around">
+			<?php foreach($posts as $post) : ?>
+				<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+				  <div class="card-header">Created on <?php echo $post['created_at']; ?> by <span class="font-weight-bold"><?php echo $post['author']; ?></span></div>
+				  <div class="card-body">
+				    <h4 class="card-title"><?php echo $post['title']; ?></h4>
+				    <p class="card-text"><?php echo $post['body']; ?></p>
+				    <a class="btn btn-danger" href="post.php?id=<?php echo $post['id'] ?>">Read More</a>
+				  </div>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+<?php require('inc/footer.php'); ?>
---
+++ post.php
@@ -0,0 +1,37 @@
+<?php 
+	//database connection
+	require('config/config.php');
+	require('config/connection.php');
+
+	//get ID
+	$id = mysqli_real_escape_string($conn, $_GET['id']);
+
+	// create query
+	$query = 'SELECT * FROM posts WHERE id = '.$id;
+
+	//get results
+	$result = mysqli_query($conn, $query);
+
+	//fetch data
+	$post = mysqli_fetch_assoc($result);
+
+	//free result
+	mysqli_free_result($result);
+
+	//close connection
+	mysqli_close($conn);
+?>
+
+<?php require('inc/header.php'); ?>
+	<div class="container text-center my-5">
+		<a class="btn btn-primary" href="<?php echo ROOT_URL; ?>">Back</a>
+		<h1><?php echo $post['title']; ?></h1>
+		<div class="card text-white bg-primary mb-3">
+		  <div class="card-header">Created on <?php echo $post['created_at']; ?> by <span class="font-weight-bold"><?php echo $post['author']; ?></span></div>
+		  <div class="card-body">
+		    <!-- <h4 class="card-title">Primary card title</h4> -->
+		    <p class="card-text"><?php echo $post['body']; ?></p>
+		  </div>
+		</div>
+	</div>
+<?php require('inc/footer.php'); ?>
